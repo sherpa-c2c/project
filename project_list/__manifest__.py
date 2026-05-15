786ded926e22169dd619c043117c75928b90ddad
Odoo Migration Factory <sherpa-c2c@users.noreply.github.com>
2026-05-15 03:04:25 +0200

    [MIG] project_list: 18.0 → 19.0

--- project_list/__manifest__.py
+++ project_list/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Projects List View",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "author": "CorporateHub, Odoo Community Association (OCA)",
